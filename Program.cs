dab23d766fdbc28c891ead978814bbbb70016a44
that guy <defender22017@gmail.com>
2024-05-13 10:51:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 101);
+
+            Console.WriteLine("Guess the number (1-100): ");
+            int input = int.Parse(Console.ReadLine());
+
+            while (input != number)
+            {
+                if (input < number)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else if (input > number)
+                {
+                    Console.WriteLine("Too high!");
+                }
+
+                Console.WriteLine("Guess the number (1-100): ");
+                input = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.ReadKey();
+        }
+    }
+}
